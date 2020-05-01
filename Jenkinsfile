f94df31bce6b17176c9df63d72766e8382bfd09d
Ashish Singh Baghel <mechashishsingh@gmail.com>
2020-05-01 20:56:15 +0200

    updated jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,7 +1,7 @@
 node{
     
     stage('git checkout'){
-        git credentialsId: 'git-creds', url: 'https://github.com/mechdeveloper/TCSDevOpsBootCamp.git'
+        git url: 'https://github.com/mechdeveloper/cicd-pipeline-war.git'
         
     }
     stage('clean up'){
@@ -14,20 +14,9 @@ node{
        def mavenCMD = "${mavenHome}/bin/mvn"
        sh "${mavenCMD} package"
     }
-    stage('build docker image'){
-       sh 'docker build -t ashishbagheldocker/cicd:2.0 .'
-    }
-    stage('Push docker image') {
-        withCredentials([string(credentialsId: 'docker-password', variable: 'dockerHubPwd')]){
-            sh "docker login -u ashishbagheldocker -p ${dockerHubPwd}"
-        }
-        sh 'docker push ashishbagheldocker/cicd:2.0'
-    }
-    stage('Pull Image') {
-        sh 'docker pull ashishbagheldocker/cicd:2.0'
-    }
-    stage('Run Container') {
-        sh 'docker run -d -p 8888:8888 ashishbagheldocker/cicd:2.0'
+    stage('Deploy Tomcat'){
+        sh "rm -r '/opt/tomcat/webapps/ROOT'"
+        sh "cp 'target/ROOT.war' '/opt/tomcat/webapps'"
     }
     
 }
--- tomcat/install-tomcat.sh
+++ tomcat/install-tomcat.sh
@@ -1,10 +1,14 @@
 # Step 1: Install OpenJDK
+
 sudo apt update
+
 sudo apt install default-jdk -y
 
 # Step 2: Create Tomcat User
 # First, create a new tomcat group:
+
 sudo groupadd tomcat
+
 # Next, create a new tomcat user. We’ll make this user a member of the tomcat group, 
 # with a home directory of /opt/tomcat (where we will install Tomcat), 
 # and with a shell of /bin/false (so nobody can log into the account):
@@ -12,14 +16,20 @@ sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
 
 # Step 3: Install Tomcat
 cd /tmp
+
 curl -O http://mirror.23media.de/apache/tomcat/tomcat-8/v8.5.30/bin/apache-tomcat-8.5.30.tar.gz
+
 sudo mkdir /opt/tomcat
+
 sudo tar xzvf apache-tomcat-*tar.gz -C /opt/tomcat --strip-components=1
 
 # Step 4: Update Permissions
 cd /opt/tomcat
+
 sudo chgrp -R tomcat /opt/tomcat
+
 sudo chmod -R g+r conf
+
 sudo chmod g+x conf
 
 sudo chown -R tomcat webapps/ work/ temp/ logs/
@@ -29,15 +39,18 @@ sudo chown -R tomcat webapps/ work/ temp/ logs/
 # JAVA_HOME
 # /usr/lib/jvm/java-1.8.0-openjdk-amd64/jre
 
-cp tomcat.service /etc/systemd/system/tomcat.service 
+cp tomcat.service /etc/systemd/system/tomcat.service
+
 cat /etc/systemd/system/tomcat.service 
 
 sudo systemctl daemon-reload
+
 sudo systemctl start tomcat
 # sudo systemctl status tomcat
 
 # Step 6: Adjust the Firewall and Test the Tomcat Server
 sudo ufw allow 8080
+
 curl http://localhost:8080
 
 sudo systemctl enable tomcat
